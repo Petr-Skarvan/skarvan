07eee3f5dbf6fc33a15e91dcaad83f8fcd147aa5
Petr Škarvan <skarv83489@mot.sps-dopravni.cz>
2026-05-15 08:20:13 +0200

    OOP

---
+++ OOP.py
@@ -0,0 +1,13 @@
+class Auto:
+  def natankuj(self):
+    print("Tankuji plnou nadrž")
+
+skoda = Auto()
+bmw = Auto()
+dacia = Auto()
+nissan = Auto()
+
+bmw.natankuj()
+skoda.natankuj()
+dacia.natankuj()
+nissan.natankuj()
